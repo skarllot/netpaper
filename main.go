0220d609e64e7abfa7ede1635111a538023d8a8b
Fabrício Godoy <skarllot@gmail.com>
2015-01-29 19:01:04 -0200

    Added a HTTP middleware to recover from errors

--- main.go
+++ main.go
@@ -47,8 +47,23 @@ func main() {
 		return
 	}
 
-	commonHandlers := alice.New(context.ClearHandler)
+	commonHandlers := alice.New(context.ClearHandler, recoverHandler)
 	router := NewRouter()
 	router.Get("/logon/hasUsers", commonHandlers.ThenFunc(appC.HasUsers))
 	http.ListenAndServe(":8080", router.httpRouter)
+}
+
+func recoverHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %+v", err)
+				http.Error(w, http.StatusText(500), 500)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
 }
